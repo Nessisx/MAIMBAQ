c7648c0492292939dd3e09a1ffd50735494493a3
Nelson Andres Sierra Julio <nelson.sierra@unisimon.edu.co>
2026-06-05 12:59:28 -0500

    Deploy website - based on 25aef199b854f85b08eb4d2eb58efe919d673e0a

---
+++ assets/js/acecf23e.ca41ca0c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([["220"],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/MAIMBAQ/blog","blogTitle":"Blog","authorsListPath":"/MAIMBAQ/blog/authors"}')}}]);
